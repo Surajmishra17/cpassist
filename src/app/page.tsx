743b64b826e0f0d2e5b4837a6cc5a9c1c0ed2a7c
GreyFox00001 <reachtanish@gmail.com>
2026-03-11 23:52:34 +0530

    LandingPage-Features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { HeroDemo } from "@/components/blocks/hero";
+import { Feature108Demo } from "@/components/blocks/feature108";
 
 export default function Home() {
   return (
     <main>
       <HeroDemo />
+      <Feature108Demo />
     </main>
   );
 }
